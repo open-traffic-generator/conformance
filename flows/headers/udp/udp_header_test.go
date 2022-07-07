56aac4d31a4d2b7df7f230242997dc99ccffc049
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-07-07 17:10:49 +0000

    refactored udp test

--- flows/headers/udp/udp_header_test.go
+++ flows/headers/udp/udp_header_test.go
@@ -8,53 +8,69 @@ import (
 )
 
 func TestUdpHeader(t *testing.T) {
-	c := otg.Api(t).NewConfig()
-	p1 := c.Ports().Add().SetName("p1").SetLocation(otg.OtgPorts(t)[0])
-	p2 := c.Ports().Add().SetName("p2").SetLocation(otg.OtgPorts(t)[1])
+	testConst := map[string]interface{}{
+		"pktRate":   int64(50),
+		"pktCount":  int32(100),
+		"pktSize":   int32(128),
+		"txMac":     "00:00:01:01:01:01",
+		"rxMac":     "00:00:01:01:01:02",
+		"txIp":      "1.1.1.1",
+		"rxIp":      "1.1.1.2",
+		"txUdpPort": int32(5000),
+		"rxUdpPort": int32(6000),
+	}
+
+	api := otg.NewOtgApi(t)
+	c := udpHeaderConfig(api, testConst)
+
+	api.SetConfig(c)
+
+	api.StartTransmit()
+
+	api.WaitFor(
+		func() bool { return udpHeaderMetricsOk(api) },
+		&otg.WaitForOpts{FnName: "WaitForFlowMetrics"},
+	)
+}
+
+func udpHeaderConfig(api *otg.OtgApi, tc map[string]interface{}) gosnappi.Config {
+	c := api.Api().NewConfig()
+	p1 := c.Ports().Add().SetName("p1").SetLocation(api.TestConfig().OtgPorts[0])
+	p2 := c.Ports().Add().SetName("p2").SetLocation(api.TestConfig().OtgPorts[1])
 
 	c.Layer1().Add().
 		SetName("ly").
 		SetPortNames([]string{p1.Name(), p2.Name()}).
-		SetSpeed(gosnappi.Layer1Speed.SPEED_1_GBPS)
+		SetSpeed(gosnappi.Layer1SpeedEnum(api.TestConfig().OtgSpeed))
 
 	f1 := c.Flows().Add().SetName("f1")
 	f1.TxRx().Port().
 		SetTxName(p1.Name()).
 		SetRxName(p2.Name())
-	f1.Duration().FixedPackets().SetPackets(100)
-	f1.Rate().SetPps(50)
-	f1.Size().SetFixed(128)
+	f1.Duration().FixedPackets().SetPackets(tc["pktCount"].(int32))
+	f1.Rate().SetPps(tc["pktRate"].(int64))
+	f1.Size().SetFixed(tc["pktSize"].(int32))
 	f1.Metrics().SetEnable(true)
 
 	eth := f1.Packet().Add().Ethernet()
-	eth.Src().SetValue("00:00:00:00:00:AA")
-	eth.Dst().SetValue("00:00:00:00:00:BB")
+	eth.Src().SetValue(tc["txMac"].(string))
+	eth.Dst().SetValue(tc["rxMac"].(string))
 
 	ip := f1.Packet().Add().Ipv4()
-	ip.Src().SetValue("1.1.1.10")
-	ip.Dst().SetValue("1.1.1.20")
+	ip.Src().SetValue(tc["txIp"].(string))
+	ip.Dst().SetValue(tc["rxIp"].(string))
 
 	udp := f1.Packet().Add().Udp()
-	udp.SrcPort().SetValue(5000)
-	udp.DstPort().SetValue(6000)
+	udp.SrcPort().SetValue(tc["txUdpPort"].(int32))
+	udp.DstPort().SetValue(tc["rxUdpPort"].(int32))
 
-	otg.SetConfig(t, c)
-
-	otg.StartTransmit(t)
-
-	err := otg.WaitFor(
-		t, udpHeaderMetricsOk, &otg.WaitForOpts{FnName: "WaitForFlowMetrics"},
-	)
-
-	if err != nil {
-		t.Fatal(err)
-	}
+	api.Testing().Logf("Config:\n%v\n", c)
+	return c
 }
 
-func udpHeaderMetricsOk(t *testing.T) (bool, error) {
-	m := otg.GetFlowMetrics(t)[0]
+func udpHeaderMetricsOk(api *otg.OtgApi) bool {
+	m := api.GetFlowMetrics()[0]
 	return m.Transmit() == gosnappi.FlowMetricTransmit.STOPPED &&
-			m.FramesTx() == 100 &&
-			m.FramesRx() == 100,
-		nil
+		m.FramesTx() == 100 &&
+		m.FramesRx() == 100
 }
