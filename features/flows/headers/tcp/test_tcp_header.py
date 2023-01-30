4ae3368ac30b9dbe15f63fdc45e97d498a9df245
Vibaswan <vroychowdhury@gmail.com>
2023-01-30 16:07:52 +0530

    adding validation for tcp data offset in tcp tests

--- features/flows/headers/tcp/test_tcp_header.py
+++ features/flows/headers/tcp/test_tcp_header.py
@@ -125,6 +125,9 @@ def capture_ok(api, c, tc):
         captured_packets.validate_field(
             "tcp dst", i, 36, api.num_to_bytes(tc["rxTcpPort"], 2)
         )
+        captured_packets.validate_field(
+            "tcp data offset", i, 46, api.num_to_bytes(5, 1)
+        )
 
     exp_count = tc["pktCount"]
     act_count = len(captured_packets.packets) - ignored_count
