34f7dfa6192f1ff20fe90048ba2e4dc2c99985e0
Ashutosh Kumar <ashutshkumr@gmail.com>
2023-02-04 05:18:09 +0000

    added LLDP neighbor test

---
+++ feature/b2b/lldp/lldp_neighbors_test.go
@@ -0,0 +1,102 @@
+//go:build all || cpdp
+
+package lldp
+
+import (
+	"testing"
+	"time"
+
+	"github.com/open-traffic-generator/conformance/helpers/otg"
+	"github.com/open-traffic-generator/snappi/gosnappi"
+)
+
+func TestLldpNeighbors(t *testing.T) {
+	testConst := map[string]interface{}{
+		"txMac":       "00:00:01:01:01:01",
+		"rxMac":       "00:00:01:01:01:02",
+		"holdTime":    int32(120),
+		"advInterval": int32(5),
+		"pduCount":    int64(2),
+	}
+
+	api := otg.NewOtgApi(t)
+	c := lldpNeighborsConfig(api, testConst)
+
+	api.SetConfig(c)
+
+	api.StartProtocols()
+
+	api.WaitFor(
+		func() bool { return lldpNeighborsLldpMetricssOk(api, testConst) },
+		&otg.WaitForOpts{FnName: "WaitForLldpMetrics", Timeout: 30 * time.Second},
+	)
+
+	api.WaitFor(
+		func() bool { return lldpNeighborsLldpNeighborsOk(api, testConst) },
+		&otg.WaitForOpts{FnName: "WaitForLldpNeighbors", Timeout: 30 * time.Second},
+	)
+}
+
+func lldpNeighborsConfig(api *otg.OtgApi, tc map[string]interface{}) gosnappi.Config {
+	c := api.Api().NewConfig()
+
+	ptx := c.Ports().Add().SetName("ptx").SetLocation(api.TestConfig().OtgPorts[0])
+	prx := c.Ports().Add().SetName("prx").SetLocation(api.TestConfig().OtgPorts[1])
+
+	c.Layer1().Add().
+		SetName("ly").
+		SetPortNames([]string{ptx.Name(), prx.Name()}).
+		SetSpeed(gosnappi.Layer1SpeedEnum(api.TestConfig().OtgSpeed))
+
+	// TODO: remove device configuration in next release of ixia-c
+	dtx := c.Devices().Add().SetName("dtx")
+	dtx.Ethernets().Add().SetPortName(ptx.Name()).SetName("dtxEth").SetMac(tc["txMac"].(string))
+	drx := c.Devices().Add().SetName("drx")
+	drx.Ethernets().Add().SetPortName(prx.Name()).SetName("drxEth").SetMac(tc["rxMac"].(string))
+
+	lldpTx := c.Lldp().Add().SetName("lldpTx")
+	lldpRx := c.Lldp().Add().SetName("lldpRx")
+
+	lldpTx.SetHoldTime(tc["holdTime"].(int32))
+	lldpTx.SetAdvertisementInterval(tc["advInterval"].(int32))
+	lldpTx.Connection().SetPortName(ptx.Name())
+	lldpTx.ChassisId().MacAddressSubtype().
+		SetValue(tc["txMac"].(string))
+
+	lldpRx.SetHoldTime(tc["holdTime"].(int32))
+	lldpRx.SetAdvertisementInterval(tc["advInterval"].(int32))
+	lldpRx.Connection().SetPortName(prx.Name())
+	lldpRx.ChassisId().MacAddressSubtype().
+		SetValue(tc["rxMac"].(string))
+
+	api.Testing().Logf("Config:\n%v\n", c)
+	return c
+}
+
+func lldpNeighborsLldpMetricssOk(api *otg.OtgApi, tc map[string]interface{}) bool {
+
+	for _, m := range api.GetLldpMetrics() {
+		// TODO: should be an equality check
+		if m.FramesTx() < tc["pduCount"].(int64) || m.FramesRx() < tc["pduCount"].(int64) {
+			return false
+		}
+
+	}
+
+	return true
+}
+
+func lldpNeighborsLldpNeighborsOk(api *otg.OtgApi, tc map[string]interface{}) bool {
+	count := 0
+	for _, n := range api.GetLldpNeighbors() {
+		for _, key := range []string{"txMac", "rxMac"} {
+			if n.HasChassisId() &&
+				n.ChassisIdType() == gosnappi.LldpNeighborsStateChassisIdType.MAC_ADDRESS &&
+				n.ChassisId() == tc[key].(string) {
+				count += 1
+			}
+		}
+	}
+
+	return count == 2
+}
