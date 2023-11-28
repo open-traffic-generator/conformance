15eedbe61cbc7dbddadb345af5a7ea0d1af70125
Ashutosh Kumar <ashutshkumr@gmail.com>
2023-11-28 14:21:47 +0530

    added perf test for ip neighbors

---
+++ performance/b2b/ip_neighbors_perf_test.go
@@ -0,0 +1,164 @@
+//go:build all || cpdp
+
+package b2b
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/open-traffic-generator/conformance/helpers/otg"
+	"github.com/open-traffic-generator/snappi/gosnappi"
+)
+
+func TestIpNeighborsPerf(t *testing.T) {
+	// TODO: add support for IPv6 as well
+	testConst := map[string]interface{}{
+		"pktRate":   uint64(50),
+		"pktCount":  uint32(100),
+		"pktSize":   uint32(128),
+		"txMac":     "00:00:01:01:01:01",
+		"txIp":      "1.1.1.1",
+		"txGateway": "1.1.1.2",
+		"txPrefix":  uint32(24),
+		"rxMac":     "00:00:01:01:01:02",
+		"rxIp":      "1.1.1.2",
+		"rxGateway": "1.1.1.1",
+		"rxPrefix":  uint32(24),
+	}
+
+	distTables := []string{}
+	testCase := fmt.Sprintf("IpNeighborsIpHeader2Ports2Devices1Flow")
+
+	api := otg.NewOtgApi(t)
+	c := ipNeighborsConfig(api, testConst)
+
+	t.Log("TEST CASE: ", testCase)
+	for i := 1; i <= api.TestConfig().OtgIterations; i++ {
+		t.Logf("ITERATION: %d\n\n", i)
+
+		api.SetConfig(c)
+
+		api.WaitFor(
+			func() bool { return ipNeighborsIpv4NeighborsOk(api, testConst) },
+			&otg.WaitForOpts{FnName: "WaitForIpv4Neighbors"},
+		)
+
+		api.StartTransmit()
+
+		api.WaitFor(
+			func() bool { return flowMetricsOk(api, testConst) },
+			&otg.WaitForOpts{FnName: "WaitForFlowMetrics"},
+		)
+		api.Plot().AppendZero()
+	}
+
+	api.LogPlot(testCase)
+
+	tb, err := api.Plot().ToTable()
+	if err != nil {
+		t.Fatal("ERROR:", err)
+	}
+	distTables = append(distTables, tb)
+
+	for _, d := range distTables {
+		t.Log(d)
+	}
+}
+
+func ipNeighborsConfig(api *otg.OtgApi, tc map[string]interface{}) gosnappi.Config {
+	c := api.Api().NewConfig()
+
+	ptx := c.Ports().Add().SetName("ptx").SetLocation(api.TestConfig().OtgPorts[0])
+	prx := c.Ports().Add().SetName("prx").SetLocation(api.TestConfig().OtgPorts[1])
+
+	c.Layer1().Add().
+		SetName("ly").
+		SetPortNames([]string{ptx.Name(), prx.Name()}).
+		SetSpeed(gosnappi.Layer1SpeedEnum(api.TestConfig().OtgSpeed))
+
+	dtx := c.Devices().Add().SetName("dtx")
+	drx := c.Devices().Add().SetName("drx")
+
+	dtxEth := dtx.Ethernets().
+		Add().
+		SetName("dtxEth").
+		SetMac(tc["txMac"].(string)).
+		SetMtu(1500)
+
+	dtxEth.Connection().SetPortName(ptx.Name())
+
+	dtxIp := dtxEth.
+		Ipv4Addresses().
+		Add().
+		SetName("dtxIp").
+		SetAddress(tc["txIp"].(string)).
+		SetGateway(tc["txGateway"].(string)).
+		SetPrefix(tc["txPrefix"].(uint32))
+
+	drxEth := drx.Ethernets().
+		Add().
+		SetName("drxEth").
+		SetMac(tc["rxMac"].(string)).
+		SetMtu(1500)
+
+	drxEth.Connection().SetPortName(prx.Name())
+
+	drxIp := drxEth.
+		Ipv4Addresses().
+		Add().
+		SetName("drxIp").
+		SetAddress(tc["rxIp"].(string)).
+		SetGateway(tc["rxGateway"].(string)).
+		SetPrefix(tc["rxPrefix"].(uint32))
+
+	flow := c.Flows().Add()
+	flow.SetName("ftxV4")
+	flow.Duration().FixedPackets().SetPackets(tc["pktCount"].(uint32))
+	flow.Rate().SetPps(tc["pktRate"].(uint64))
+	flow.Size().SetFixed(tc["pktSize"].(uint32))
+	flow.Metrics().SetEnable(true)
+
+	flow.TxRx().Device().
+		SetTxNames([]string{dtxIp.Name()}).
+		SetRxNames([]string{drxIp.Name()})
+
+	ftxV4Eth := flow.Packet().Add().Ethernet()
+	ftxV4Eth.Src().SetValue(dtxEth.Mac())
+
+	ftxV4Ip := flow.Packet().Add().Ipv4()
+	ftxV4Ip.Src().SetValue(tc["txIp"].(string))
+	ftxV4Ip.Dst().SetValue(tc["rxIp"].(string))
+
+	api.Testing().Logf("Config:\n%v\n", c)
+	return c
+}
+
+func flowMetricsOk(api *otg.OtgApi, tc map[string]interface{}) bool {
+	pktCount := uint64(tc["pktCount"].(uint32))
+
+	for _, m := range api.GetFlowMetrics() {
+		if m.Transmit() != gosnappi.FlowMetricTransmit.STOPPED ||
+			m.FramesTx() != pktCount ||
+			m.FramesRx() != pktCount {
+			return false
+		}
+
+	}
+
+	return true
+}
+
+func ipNeighborsIpv4NeighborsOk(api *otg.OtgApi, tc map[string]interface{}) bool {
+	count := 0
+	for _, n := range api.GetIpv4Neighbors() {
+		if n.HasLinkLayerAddress() {
+			for _, key := range []string{"txGateway", "rxGateway"} {
+				if n.Ipv4Address() == tc[key].(string) {
+					count += 1
+				}
+			}
+		}
+	}
+
+	return count == 2
+}
