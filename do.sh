2fbd65712e17c0d14371af13919d1ac1fe706bee
biplamal <biplab.mal@keysight.com>
2025-02-11 08:23:37 +0000

    Release v1.20.0-6

--- do.sh
+++ do.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # update for any release using
-# curl -kLO https://github.com/open-traffic-generator/ixia-c/releases/download/v1.20.0-3/versions.yaml
+# curl -kLO https://github.com/open-traffic-generator/ixia-c/releases/download/v1.20.0-6/versions.yaml
 VERSIONS_YAML="versions.yaml"
 VETH_A="veth-a"
 VETH_Z="veth-z"
